cf50eb9d06f7805b27d28d684b3f335f517f9ed4
Kaine69 <118997628+Kaine69@users.noreply.github.com>
2024-02-22 14:15:51 +0700

    kenapa merah semua woi?

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "s16042104week1"
+include(":app")
